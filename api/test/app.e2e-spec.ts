b4828ce49836b7912ac15d4b2ca9169f8750b0e7
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-08-03 18:02:41 -0300

    feat: adicionado teste e2e da rota search-github

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TakeblipResponseDto } from 'src/search-github/dto/takeblip-response.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,71 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/search-github (GET)', () => {
+    const apiResponse: TakeblipResponseDto = {
+      itemType: 'application/vnd.lime.document-select+json',
+      items: [
+        {
+          header: {
+            type: 'application/vnd.lime.media-link+json',
+            value: {
+              title: 'takenet/library.data',
+              text: 'Provides a simple abstraction for implementing the repository and unit of work patterns for data-enabled applications',
+              type: 'image/jpeg',
+              uri: 'https://avatars.githubusercontent.com/u/4369522?v=4',
+            },
+          },
+        },
+        {
+          header: {
+            type: 'application/vnd.lime.media-link+json',
+            value: {
+              title: 'takenet/library.logging',
+              text: 'Provides a simple logging interface for applications and some basic implementations of this interface',
+              type: 'image/jpeg',
+              uri: 'https://avatars.githubusercontent.com/u/4369522?v=4',
+            },
+          },
+        },
+        {
+          header: {
+            type: 'application/vnd.lime.media-link+json',
+            value: {
+              title: 'takenet/CorrectNumber',
+              text: 'This project have the single propose of creating a simple rest service to format telephone numbers',
+              type: 'image/jpeg',
+              uri: 'https://avatars.githubusercontent.com/u/4369522?v=4',
+            },
+          },
+        },
+        {
+          header: {
+            type: 'application/vnd.lime.media-link+json',
+            value: {
+              title: 'takenet/libphonenumber-csharp',
+              text: 'Forking original c# port',
+              type: 'image/jpeg',
+              uri: 'https://avatars.githubusercontent.com/u/4369522?v=4',
+            },
+          },
+        },
+        {
+          header: {
+            type: 'application/vnd.lime.media-link+json',
+            value: {
+              title: 'takenet/Takenet.Radar',
+              text: 'Radar de tecnologias takenet',
+              type: 'image/jpeg',
+              uri: 'https://avatars.githubusercontent.com/u/4369522?v=4',
+            },
+          },
+        },
+      ],
+    };
+
     return request(app.getHttpServer())
-      .get('/')
+      .get('/search-github')
       .expect(200)
-      .expect('Hello World!');
+      .expect(apiResponse);
   });
 });
