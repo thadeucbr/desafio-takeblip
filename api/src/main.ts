04625dc2c2408e62e7fe33c35ecf2057574f9f75
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-08-03 18:03:02 -0300

    feat: adicionada configuracao swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio Take Blip')
+    .setDescription(
+      'Api referente ao teste tecnico para desenvolvedor na Take Blip.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
