9e44669b11a45cf7aebd3db644deaf9d667b1867
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-08-03 18:01:28 -0300

    feat: adicionando a importacao do SearchGithubModule ao app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SearchGithubModule } from './search-github/search-github.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SearchGithubModule],
 })
 export class AppModule {}
